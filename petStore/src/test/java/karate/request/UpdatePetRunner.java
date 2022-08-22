04e6493925280014faa6297434520b4f1c08a931
Edwin Cuadro <cuadroedwin10@gmail.com>
2022-08-22 14:55:41 -0500

    Adding modifications

--- petStore/src/test/java/karate/request/UpdatePetRunner.java
+++ petStore/src/test/java/karate/request/UpdatePetRunner.java
@@ -4,6 +4,6 @@ import com.intuit.karate.junit5.Karate;
 public class UpdatePetRunner {
     @Karate.Test
     Karate updatePet(){
-        return Karate.run("classpath:karate/features/2updatePet.feature").relativeTo(getClass());
+        return Karate.run("classpath:karate/features/3updatePet.feature").relativeTo(getClass());
     }
 }
